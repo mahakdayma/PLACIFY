a92f04d6edc21c9e21f4c65bc6b41d1aa87cbf9d
mahakdayma <daymamahak@gmail.com>
2025-11-09 17:13:11 +0530

    Create Home component for the main page

---
+++ app/app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>Placify</h1>
+      <p style={{color:"#888"}}>Path to Placement</p>
+    </div>
+  );
+}
